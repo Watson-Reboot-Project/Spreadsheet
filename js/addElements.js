f585f5d135d14eaac575a4b694534ef6642f1323
James Miltenberger <jamilt13@gmail.com>
2014-02-27 20:18:21 -0600

    created basic table

--- js/addElements.js
+++ js/addElements.js
@@ -1,4 +1,13 @@
 /*
  * This handles adding all elements to the document body
  * */
+var figNum = 0;
 
+//Div for spreadsheet grid
+var tableDiv = document.createElement('div');
+tableDiv.id = "table" + figNum;
+
+
+
+var container = document.getElementById("container");
+container.appendChild(tableDiv);
--- js/table.js
+++ js/table.js
@@ -1,3 +1,12 @@
 /*
  * This handles the functionality of the table
  * */
+
+
+$('#' + tableDiv.id).handsontable({
+  minSpareRows: 20,
+  minSpareCols: 20,
+  rowHeaders: true,
+  colHeaders: true,
+  contextMenu: true
+});
